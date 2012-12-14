7378a1f7eeb34574c8b6f39b4de821041ca0d11e
dacaiguoguo <dacaiguoguo@163.com>
2012-12-14 17:34:17 +0800

    iss01 branch first commit

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(){
 	printf("hello world I'm coming~");
 	printf("you are mine!,oh, no");
 	printf("you are mine!\n");
+	printf("iss01 branch");
 	printf("my name is syg.developer");
 	return 0;
 }
